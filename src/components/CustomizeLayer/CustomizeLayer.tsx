b95dc3bc2a22ec417d68554d89012652616b141b
Mohamed Ashraf <92171438+devmohamedashraf@users.noreply.github.com>
2024-12-03 03:44:50 +0200

    Fix remaining bugs (#35)
    * Fix remaining bugs
    
    * Fix remaining bugs

--- src/components/CustomizeLayer/CustomizeLayer.tsx
+++ src/components/CustomizeLayer/CustomizeLayer.tsx
@@ -121,8 +121,8 @@ function CustomizeLayer() {
           <SavedIconFeedback />
         </div>
       ) : (
-        <div className="flex flex-col lg:pr-2 w-full h-full">
-          <div className="w-full h-full px-4 py-4">
+        <div className="flex flex-col lg:pr-2 w-full h-full overflow-hidden">
+          <div className="w-full lg:h-full px-4 py-4 flex-1 ">
             {error && (
               <div className="mt-2 mb-2 text-red-500 font-semibold">
                 {error}
@@ -189,11 +189,11 @@ function CustomizeLayer() {
               />
             </div>
           </div>
-          <div className="w-full h-[7%] flex  px-2 py-2 select-none border-t lg:mb-0 mb-16">
-            <div className="flex h-full w-full space-x-2">
+          <div className="w-full lg:h-[7%] flex  px-2 py-2 select-none border-t lg:mb-0 mb-16">
+            <div className="flex lg:h-full w-full space-x-2">
               <button
                 onClick={handleDiscardClick}
-                className="w-full h-full bg-slate-100 border-2 border-[#115740] text-[#115740] flex justify-center items-center font-semibold rounded-lg
+                className="w-full h-10 bg-slate-100 border-2 border-[#115740] text-[#115740] flex justify-center items-center font-semibold rounded-lg
                hover:bg-white transition-all cursor-pointer"
               >
                 Discard
@@ -205,7 +205,7 @@ function CustomizeLayer() {
                   handleButtonClick(e);
                 }}
                 disabled={showLoaderTopup}
-                className="w-full h-full bg-[#115740] text-white flex justify-center items-center font-semibold rounded-lg hover:bg-[#123f30] transition-all cursor-pointer"
+                className="w-full h-10 bg-[#115740] text-white flex justify-center items-center font-semibold rounded-lg hover:bg-[#123f30] transition-all cursor-pointer"
               >
                 {showLoaderTopup ? "Loading..." : "Save"}
               </button>
