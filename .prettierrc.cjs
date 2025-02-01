b5e9ac0ab0256c7490e1f4969f4e781067f7e602
“Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2025-02-01 13:14:10 +0100

    fix: prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 100,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+} 
